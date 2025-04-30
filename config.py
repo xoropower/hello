435c6f13cc2ed448623a3cab1e98d8ae07b6d6c0
xoropower <hii518445@gmail.com>
2025-04-30 13:42:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-api_id = "enter your api_id"
-api_hash = "enter your api_hash"
+api_id = "25566754"
+api_hash = "27609daf8ee847978797a359024e7607"
 bot_token = "enter your bot token"
-auth_users = [7991495621]
+auth_users = []
